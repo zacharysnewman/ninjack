8427a44e3d6ec021db68c14fd409864b7c03f367
Zack <51299535+zacharysnewman@users.noreply.github.com>
2024-11-23 13:16:43 -0800

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -549,13 +549,22 @@ function main() {
 	document.addEventListener('keydown', onKeyDown);
 	let lastTouchTime = 0;
 
-document.addEventListener('touchend', (e) => {
-  const now = new Date().getTime();
-  if (now - lastTouchTime <= 300) {
-    e.preventDefault(); // Prevent double-tap-to-zoom
-  }
-  lastTouchTime = now;
-});
+(function($) {
+  $.fn.nodoubletapzoom = function() {
+      $(this).bind('touchstart', function preventZoom(e) {
+        var t2 = e.timeStamp
+          , t1 = $(this).data('lastTouch') || t2
+          , dt = t2 - t1
+          , fingers = e.originalEvent.touches.length;
+        $(this).data('lastTouch', t2);
+        if (!dt || dt > 500 || fingers > 1) return; // not double-tap
+
+        e.preventDefault(); // double tap - prevent the zoom
+        // also synthesize click events we just swallowed up
+        $(this).trigger('click').trigger('click');
+      });
+  };
+})(jQuery);
 	resetGame();
 	alert(alertMessages.welcome);
 }
