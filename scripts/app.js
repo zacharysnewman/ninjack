18e57da2ca8c6460a4dbda77280e1ae51e242343
Zack <51299535+zacharysnewman@users.noreply.github.com>
2024-11-23 16:37:37 -0800

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -547,13 +547,6 @@ const onKeyDown = (event) => {
 
 function main() {
 	document.addEventListener('keydown', onKeyDown);
-$('.no-zoom').bind('touchend', function(e) {
-  e.preventDefault();
-  // Add your code here. 
-  $(this).click();
-  // This line still calls the standard click event, in case the user needs to interact with the element that is being clicked on, but still avoids zooming in cases of double clicking.
-})
-
 	resetGame();
 	alert(alertMessages.welcome);
 }
